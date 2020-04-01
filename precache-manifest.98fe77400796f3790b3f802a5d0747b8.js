7f96312dab037aad7517de78b12efe921627f732
kimthiendung90 <kimthiendung@gmail.com>
2020-04-01 13:48:53 +0700

    up

---
+++ precache-manifest.98fe77400796f3790b3f802a5d0747b8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a916edc1c7089ded15c0",
+    "url": "/css/about.591d0ae7.css"
+  },
+  {
+    "revision": "d9f0bdfaedc2317ef7da",
+    "url": "/css/app.c9b60859.css"
+  },
+  {
+    "revision": "cd4fbcb8cbb6b9df64b8",
+    "url": "/css/chunk-vendors.a8bdcea0.css"
+  },
+  {
+    "revision": "01df170306275f4440f6bfe0919d30f1",
+    "url": "/css/flexboxgrid.min.css"
+  },
+  {
+    "revision": "c7fd7e2cbe0bc11361c5374c215f84e9",
+    "url": "/img/800x450/1.jpg"
+  },
+  {
+    "revision": "8bc25066dcfe13b85dd9346143334650",
+    "url": "/img/800x450/2.jpg"
+  },
+  {
+    "revision": "ae42ff815b6a0b0c5f2d90bf1a8c1411",
+    "url": "/img/800x450/3.jpg"
+  },
+  {
+    "revision": "a680e22298cc79fbce62ada66ab6a91e",
+    "url": "/img/800x450/4.jpg"
+  },
+  {
+    "revision": "372035643b1ce6a9cf70ffbc3b2a81b9",
+    "url": "/img/800x450/5.jpg"
+  },
+  {
+    "revision": "2578be843dbd81f0b6d7f3411d3f8721",
+    "url": "/img/800x450/6.jpg"
+  },
+  {
+    "revision": "840060c635e0f503200a8897daa3668b",
+    "url": "/img/800x800/cappuccino.jpg"
+  },
+  {
+    "revision": "2c5d6ada3c1e7e919aa0de47e8c6580b",
+    "url": "/img/800x800/carmel-frappe.jpg"
+  },
+  {
+    "revision": "54ddf3b4abf1900c563dba3a0c2ce7bc",
+    "url": "/img/800x800/freddo.jpg"
+  },
+  {
+    "revision": "fc0a016c474963d0208d89d4269d1d42",
+    "url": "/img/800x800/glace.jpg"
+  },
+  {
+    "revision": "cbd84e5263719a0102f0357b66b501a0",
+    "url": "/img/800x800/latte-macchiato.jpg"
+  },
+  {
+    "revision": "fabd6a7a70a190b258d682f2a6b9a400",
+    "url": "/img/800x800/mocha.jpg"
+  },
+  {
+    "revision": "335e2ca7b8f362e6efd2611cdc86928e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a916edc1c7089ded15c0",
+    "url": "/js/about.7a3bf97f.js"
+  },
+  {
+    "revision": "d9f0bdfaedc2317ef7da",
+    "url": "/js/app.71f996f9.js"
+  },
+  {
+    "revision": "cd4fbcb8cbb6b9df64b8",
+    "url": "/js/chunk-vendors.a76f01f3.js"
+  },
+  {
+    "revision": "e9c64afa3ba880d029de24ba203ebfca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
